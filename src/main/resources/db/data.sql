f1f8f26c2a278f013f12432ee0e15e0eff4a1521
unikyul <zldzkdbs97@gmail.com>
2024-09-11 18:40:00 +0900

    테스트입니다

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,2 +1,4 @@
 insert into user_tb(login_id, password, img, nick_name, email, phone)
 values ('ssar', '1234',null,'hapssar', 'ssar@nate.com','010-1234-5678');
+insert into user_tb(login_id, password, img, nick_name, email, phone)
+values ('cos', '1234',null,'cosser', 'cos@nate.com','010-1234-1234');
